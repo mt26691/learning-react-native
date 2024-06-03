6778a2c5b342fee659b6d4ddebd2ca513cd2ff18
Tung Nguyen <tung.nguyen@workitspaces.com.au>
2024-06-04 08:48:11 +1000

    reset project

--- app-example/(tabs)/index.tsx
+++ app-example/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello World!</ThemedText>
+        <ThemedText type="title">Hello Hai Tết!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
