0c375eab153646dd4bef571b25110cf094a528c5
Tung Nguyen <tung.nguyen@workitspaces.com.au>
2024-06-06 08:17:45 +1000

    add navigation

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function HomeScreen() {
-  const navigation = useNavigation();
+export type RootStackParamList = {
+  Home: undefined;
+  List: undefined;
+};
 
+type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
   return (
     <View>
       <Text>Home Screen</Text>
+      <Button title="Go to List" onPress={() => navigation.navigate('List')} />
     </View>
   );
 }
